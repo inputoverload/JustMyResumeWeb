2bce02580e6f7e9932fd1327942ab339ba541f53
inputoverload <48261553+inputoverload@users.noreply.github.com>
2019-04-16 11:31:44 -0500

    Got list of resumes working as a dialog and passed the JWT across the link.

--- ClientApp/src/app/browser-users/browser-users.component.ts
+++ ClientApp/src/app/browser-users/browser-users.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { User } from '../models/user';
 import { UserService } from '../dataServices/user.service';
+import { LoginService } from '../dataServices/login.service';
+import { MatDialog, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-browser-users',
@@ -9,19 +12,36 @@ import { UserService } from '../dataServices/user.service';
   styleUrls: ['./browser-users.component.css']
 })
 export class BrowserUsersComponent implements OnInit {
+  JWT: string;
+
   users: User[];
   columnsToDisplay: string[];
 
-  constructor(private userService: UserService) { }
+  constructor(
+    public dialogRef: MatDialogRef<BrowserUsersComponent>,
+    private userService: UserService,
+    private loginService: LoginService,
+    private router: Router
+  ) {
+    this.JWT = this.loginService.JWT;
+  }
 
   async ngOnInit() {
     try {
-      this.columnsToDisplay = ['lastName', 'firstName', 'city', 'state'];
+      this.columnsToDisplay = ['lastName', 'firstName', 'city', 'state', 'actions'];
 
       this.users = await this.userService.getUsers();
     } catch (error) {
       console.warn(`An error occurred while loading the resumes: ${error.message}`);
     }
+  }
+
+  editItem(id: number) {
+    console.warn(this.loginService.JWT);
+  }
+
+  preview(id: number) {
+    this.dialogRef.close({Id: id, Jwt: this.JWT, Action: 'preview');
   }
 
 }
--- ClientApp/src/app/resume-summary/resume-summary.component.ts
+++ ClientApp/src/app/resume-summary/resume-summary.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router, NavigationExtras, NavigationStart } from '@angular/router';
 import { Location } from '@angular/common';
 import { MaterialModule } from '../material'; 
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Observable } from 'rxjs/Observable';
+import { filter, map } from 'rxjs/operators';
+
 import { LoginService } from '../dataServices/login.service';
+import { BrowserUsersComponent } from '../browser-users/browser-users.component';
+import { UserComponent } from '../user/user.component';
 
 
 @Component({
@@ -12,21 +17,29 @@ import { LoginService } from '../dataServices/login.service';
   styleUrls: ['./resume-summary.component.css']
 })
 export class ResumeSummaryComponent implements OnInit {
+  state$: Observable<object>;
   userId: number;
 
-  get WebToken {
-    return localStorage.getItem("jwt");
+  get WebToken() {
+    return this.loginService.JWT;
   }
 
   constructor(
     private activeRoute: ActivatedRoute, 
     private location: Location,
     private router: Router,
-    private loginService: LoginService
+    private loginService: LoginService,
+    public dialog: MatDialog
   ) { }
 
   ngOnInit() {
     this.userId = +this.activeRoute.snapshot.paramMap.get('id');
+
+    let jwt: string;
+    this.activeRoute.fragment.subscribe(item => jwt = item);
+    if (jwt != null && this.loginService.JWT == null) {
+      this.loginService.JWT = jwt;
+    }
   }
 
   editUser(): void {
@@ -34,16 +47,33 @@ export class ResumeSummaryComponent implements OnInit {
   }
 
   logout(): void {
-    localStorage.removeItem("jwt");
     this.router.navigateByUrl("/login");
   }
 
   isLoggedIn(): boolean {
-    return (localStorage.getItem("jwt") ? true : false);
+    return (this.loginService.JWT ? true : false);
   }
 
   displayBrowser(): void {
-    this.router.navigateByUrl("/resumes");
+    const dialogRef = this.dialog.open(BrowserUsersComponent, {
+      width: '600px',
+      data: { JWT: this.loginService.JWT }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      //alert(JSON.stringify(result));
+      if (result.Action == 'preview') {
+        this.previewItem(result.Id, result.Jwt);
+      }
+    });
+  }
+
+  previewItem(id: number, JWT: string) {
+    //this.router.navigate([ResumeSummaryComponent, id], new NavigationExtras[] );
+  }
+
+  editItem(id: number) {
+
   }
 
   addNew(): void {
