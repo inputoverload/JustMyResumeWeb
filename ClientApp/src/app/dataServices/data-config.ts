5536dcf6cc7c75c2ceff9f5d31192f480c61a745
inputoverload <48261553+inputoverload@users.noreply.github.com>
2019-04-17 20:18:28 -0500

    accidental edit

--- ClientApp/src/app/dataServices/data-config.ts
+++ ClientApp/src/app/dataServices/data-config.ts
@@ -2,6 +2,6 @@ import { HttpHeaders } from "@angular/common/http";
 
 export class DataConfigModule
 {
-  public static SERVER = 'https://localhost:44396';
-
+  //public static SERVER = 'https://localhost:44396';
+  public static SERVER = 'https://justmyresumeapi20190417045524.azurewebsites.net';
 }
